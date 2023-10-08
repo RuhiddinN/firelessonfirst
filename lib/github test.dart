dc06701dc033355f5e50f806e15ce6244f1ad6ed
RuhiddinN <nuriddinovruhiddin1007@gmail.com>
2023-10-08 12:34:29 +0500

    git test screen added

---
+++ lib/github test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GithubTestScreen extends StatefulWidget {
+  const GithubTestScreen({super.key});
+
+  @override
+  State<GithubTestScreen> createState() => _GithubTestScreenState();
+}
+
+class _GithubTestScreenState extends State<GithubTestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
